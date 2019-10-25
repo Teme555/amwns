7f6e4330f91c4484eee6aa909798d1a92bed2d3d
Teme555 <56640648+Teme555@users.noreply.github.com>
2019-10-25 20:25:06 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@
         var objIMG = new Image();
         var startTime = new Date();
         objIMG.onload = objIMG.onerror = function() {
-            var delay = new Date() - startTime + 9;
+            var delay = new Date()  + 5;
             domainarr.push({ times: delay, domain: url });
             checkedNum++;
         };
